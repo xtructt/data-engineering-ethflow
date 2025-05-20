8d1a8f7774cfc74afab39137a81c49d2decf6ef6
Truc Nguyen <xtruc.tt@gmail.com>
2025-05-20 10:50:06 +1000

    add an example dag

---
+++ dags/test_dag.py
@@ -0,0 +1,11 @@
+ import datetime
+
+ from airflow.sdk import DAG
+ from airflow.providers.standard.operators.empty import EmptyOperator
+
+ with DAG(
+     dag_id="my_dag_name",
+     start_date=datetime.datetime(2021, 1, 1),
+     schedule="@daily",
+ ):
+     EmptyOperator(task_id="task")
